e7683557e1751c06291f7b68146843e8e55ce19c
Steve Choi <stevechoi93@gmail.com>
2019-03-02 22:38:16 -0800

    Updates

---
+++ precache-manifest.b255a5873f1cff2d172897a75c2a8252.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9b74b0c74fde206af4b",
+    "url": "/devQuiz/static/css/main.1a5729a9.chunk.css"
+  },
+  {
+    "revision": "e9b74b0c74fde206af4b",
+    "url": "/devQuiz/static/js/main.e9b74b0c.chunk.js"
+  },
+  {
+    "revision": "596a357797d5c74cbdc0",
+    "url": "/devQuiz/static/js/1.596a3577.chunk.js"
+  },
+  {
+    "revision": "57e6123d7eac44f6801e",
+    "url": "/devQuiz/static/js/runtime~main.57e6123d.js"
+  },
+  {
+    "revision": "26a015f47738d7a2f9e8265248284c69",
+    "url": "/devQuiz/index.html"
+  }
+];
